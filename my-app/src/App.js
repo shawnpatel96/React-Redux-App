5547346907f88712a4b0b465c2c006021b30d0c0
shawn <shawnpatel196@gmail.com>
2020-02-19 20:03:39 -0600

    finsihed styling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,8 +6,24 @@ import './App.css';
 import List from "./components/List"
 import Axios from 'axios';
 
+import styled from "styled-components"
 
 
+const Header =styled.h1`
+font-family: Georgia, serif;
+font-size: 50px;
+letter-spacing: 2px;
+word-spacing: 2px;
+color: #000000;
+font-weight: normal;
+text-decoration: none;
+font-style: normal;
+font-variant: normal;
+text-transform: none;
+background-color:black;
+color:white;
+
+`
 
 
 function App() {
@@ -20,7 +36,7 @@ function App() {
   return (
    
     <div className="App">
-      
+        <Header>THE ULTIMATE(kidding)SPECIES(dogs are better)</Header>
       <List/>
     </div>
    
--- my-app/src/components/List.js
+++ my-app/src/components/List.js
@@ -1,19 +1,67 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchData } from "../actions/actions"
+import styled from "styled-components"
+
+
+const Button = styled.button`
+background-color: #4CAF50;
+border: none;
+color: white;
+padding: 15px 32px;
+text-align: center;
+text-decoration: none;
+display: inline-block;
+font-size: 16px;
+margin: 4px 2px;
+cursor: pointer;`
+
+
+const Card =styled.div`
+height:25%;
+width:25%;
+margin-left:37.5%;
+-webkit-box-shadow: 5px 5px 15px 5px #000000; 
+box-shadow: 5px 5px 15px 5px #000000;
+margin-top:3%;
+
+
+`
+const Text = styled.h1`
+font-family: "Courier New", Courier, monospace;
+font-size: 25px;
+letter-spacing: 2px;
+word-spacing: 2px;
+color: #000000;
+font-weight: normal;
+text-decoration: none;
+font-style: normal;
+font-variant: normal;
+text-transform: none;
+color:white;
+`
+
+
+
 
 
 const Fact = props =>{
   console.log('from List Fact Function', props)
   return(
     <div>
-      <h1>THE ULTIMATE(kidding)SPECIES(dogs are better)</h1>
-      <button onClick={props.fetchData}>Click here to become a Dog person</button>
+    
+      
+      <Button onClick={props.fetchData}>Click here to become a Dog person</Button>
+      
     
       {props.isFetchingData && (
-        <h1>FETCHING CAT FACTS</h1>
+        <h1></h1>  // you can add a loading image or what ever here but API loads so fast it looks trippy
       )}
-      {props.fact && !props.isFetchingData && <h2>{props.fact.text}</h2>}
+  
+        <Card>
+        {props.fact && !props.isFetchingData && <Text>{props.fact.text}</Text>}
+        </Card>
+      
     </div>
   )
 };
@@ -27,3 +75,17 @@ const mapStateToProps = state=>{
 }
 
 export default connect( mapStateToProps, {fetchData})(Fact)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
